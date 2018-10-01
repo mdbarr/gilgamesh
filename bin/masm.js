788a54aa3891f771d805b0a4344cd66de4fb18ff
Mark Barr <mark@hyperingenuity.com>
2018-09-30 23:23:42 -0400

    Start of assembler command line tool.

--- bin/masm.js
+++ bin/masm.js
@@ -1,3 +1,10 @@
 #!/usr/bin/env node
 
 'use strict';
+
+const minimist = require('minimist');
+const options = minimist(process.argv.slice(2));
+
+const chipset = options.chipset || 'm1047';
+
+console.log(`${ chipset } assembler`);
